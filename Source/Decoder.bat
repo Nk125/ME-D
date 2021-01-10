fcc3077f48783f25d4662564108e1e8384bfdc98
nk125 <73507947+Nk125@users.noreply.github.com>
2021-01-09 20:58:05 -0600

    Create Decoder.bat

---
+++ Source/Decoder.bat
@@ -0,0 +1,11 @@
+@echo off
+:D
+echo Drag the file to the prompt and press enter
+set /p ftd=
+set /p key="Insert the key="
+%extd% /rc4 meaesrc4.txt meaes.txt %key%
+%extd% /aesdecode meaes.txt me.txt %key%
+certutil -decode me.txt m.txt
+echo S, D
+del me.txt /f /s /q & del meaes.txt /f /s /q & del meaesrc4.txt /f /s /q
+goto D
