cbda900bdd04f8cbf6f2a5dd1c13bb9bf38a67af
nk125 <nekeselgamer@gmail.com>
2021-02-14 10:45:02 -0600

    Update Decoder.bat

--- Source/Decoder.bat
+++ Source/Decoder.bat
@@ -4,8 +4,8 @@ echo Drag the file to the prompt and press enter
 set /p ftd=
 set /p key="Insert the key="
 %extd% /rc4 %ftd% meaes.txt %key%
-%extd% /aesdecode meaes.txt decoded.%ftd% %key%
-certutil -decode me.txt m.txt
+%extd% /aesdecode meaes.txt me.txt %key%
+certutil -decode me.txt decoded.%ftd%
 echo S, D
 del me.txt /f /s /q & del meaes.txt /f /s /q & del meaesrc4.txt /f /s /q
 goto D
