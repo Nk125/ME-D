094557b5e51fe9fcc759d16b5e4f847150ebdba6
nk125 <nekeselgamer@gmail.com>
2021-02-14 10:47:01 -0600

    Update Decoder.bat

--- Source/Decoder.bat
+++ Source/Decoder.bat
@@ -6,6 +6,6 @@ set /p key="Insert the key="
 %extd% /rc4 %ftd% meaes.txt %key%
 %extd% /aesdecode meaes.txt me.txt %key%
 certutil -decode me.txt decoded.%ftd%
-echo S, D
+echo Succesfully Decoded, view at %cd%\decoded.%ftd%
 del me.txt /f /s /q & del meaes.txt /f /s /q & del meaesrc4.txt /f /s /q
 goto D
