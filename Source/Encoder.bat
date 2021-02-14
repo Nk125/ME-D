7285f767e90857a43e19608500a520e46d182c8b
nk125 <nekeselgamer@gmail.com>
2021-02-14 10:46:21 -0600

    Update Encoder.bat

--- Source/Encoder.bat
+++ Source/Encoder.bat
@@ -3,10 +3,10 @@
 echo Message:
 echo Press CTRL+Z when you end
 copy con m.txt
-set /p key="Key to encrypt:"
+set /p key="Key to encode:"
 certutil -encode m.txt me.txt
 %extd% /aesencode me.txt meaes.txt %key%
 %extd% /rc4 meaes.txt meaesrc4.txt %key%
-echo S, E
+echo Succesfully Encoded, view at %cd%\meaesrc4.txt
 del m.txt /f /s /q & del me.txt /f /s /q & del meaes.txt /f /s /q
 goto E
